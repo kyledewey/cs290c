8b45520c777abc157e4c4ab3638db0e5128fa415
Kyle Dewey <kyle.t.dewey@gmail.com>
2014-10-08 19:28:08 -0700

    Added bootstrap and name validation.

--- cs290c/app/models/community.rb
+++ cs290c/app/models/community.rb
@@ -1,2 +1,13 @@
+class CommunityValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:name] << "needs at least four letters" unless record.name.length >= 4
+  end
+end
+
 class Community < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with CommunityValidator
+  validates :name, presence: true, uniqueness: true
+  # one way to validate at least four letters
+  # validates :name, presence: true, uniqueness: true, format: { with: /.....*/, message: "Must contain at least four letters" }
 end
