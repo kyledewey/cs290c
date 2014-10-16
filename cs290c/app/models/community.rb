8a3538de4c5a6cd32d7f3d41a18801ec579142bb
Kyle Dewey <kyle.t.dewey@gmail.com>
2014-10-15 18:36:19 -0700

    Added another way to do name validation.

--- cs290c/app/models/community.rb
+++ cs290c/app/models/community.rb
@@ -8,6 +8,10 @@ class Community < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with CommunityValidator
   validates :name, presence: true, uniqueness: true
+
   # one way to validate at least four letters
   # validates :name, presence: true, uniqueness: true, format: { with: /.....*/, message: "Must contain at least four letters" }
+
+  # Bryce's way:
+  # validates :name, :length { minimum: 4 }
 end
