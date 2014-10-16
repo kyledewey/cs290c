2f7fd0a786b485e617cc3184a529436b0e66bb96
Kyle Dewey <kyle.t.dewey@gmail.com>
2014-10-15 19:16:41 -0700

    Added URL validation.

--- cs290c/app/models/submission.rb
+++ cs290c/app/models/submission.rb
@@ -2,4 +2,5 @@ class Submission < ActiveRecord::Base
   belongs_to :community
   validates :title, length: { minimum: 4 }
   validates :community, presence: true
+  validates :url, :url => true
 end
